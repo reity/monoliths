f5951fe354eb32a7cefadd6964a5c38e9ae9bcc5
Andrei Lapets <a@lapets.io>
2020-07-06 17:25:19 -0400

    Additional methods for building merged modules.

--- monoliths/monoliths.py
+++ monoliths/monoliths.py
@@ -12,10 +12,12 @@ class monoliths():
     Instance of a single-file library module.
     """
 
-    def __init__(self: monoliths):
+    def __init__(self: monoliths, paths=None):
         """Create empty base module."""
         self.paths = []
         self.modules = []
+        if paths is not None:
+            self.extend(paths)
 
     def append(self: monoliths, path: str) -> monoliths:
         """Add a module to current module being assembled."""
@@ -27,6 +29,17 @@ class monoliths():
         """Add multiple modules to current module being assembled."""
         for path in paths:
             self.append(path)
+
+    def concat(self: monoliths, other: monoliths) -> monoliths:
+        """Concatenate two modules, returning a new module."""
+        m = monoliths()
+        m.paths = list(self.paths) + list(other.paths)
+        m.modules = list(self.modules) + list(other.modules)
+        return m
+
+    def __add__(self: monoliths, other: monoliths) -> monoliths:
+        """Concatenate two modules, returning a new module."""
+        return self.concat(other)
 
     def emit(self: monoliths, path: str, transform_str=None):
         """Emit assembled module as a file with specified path."""
