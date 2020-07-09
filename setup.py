767876386a738989440e04242e7c659467908086
Andrei Lapets <a@lapets.io>
2020-07-09 08:55:06 -0400

    Update version to reflect new backwards-compatible API features.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
     name="monoliths",
-    version="0.1.0",
+    version="0.2.0",
     packages=["monoliths",],
     install_requires=[],
     license="MIT",
